db671c7d290ea334090c396b050d0dcd7e195847
Jdog6114 <32915521+jason2g6114@users.noreply.github.com>
2017-10-18 19:06:55 -0400

    Initial commit

---
+++ File Scripts/File Rename.ps1
@@ -0,0 +1,13 @@
+﻿$path = "" 
+$filepath = Get-ChildItem $path
+$SerioesName = ""
+
+ForEach ($file in $filepath)
+{
+$pieces = $file.FullName.Split(".").ToUpper()
+$filename = $pieces[1] 
+$extensionlocation = $pieces.count
+$extension = $pieces[$extensionlocation-1]
+Write-Host "File name will be $SeriesName  $filename.$extension"
+Rename-Item -Path "$path\$file" -NewName "$SeriesName $filename.$extension"
+}
